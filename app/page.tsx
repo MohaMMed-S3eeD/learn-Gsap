f86eda70faf47aab789e1e2f4c4b9a8bf791f976
Mohamed Seed <mosaeed7888@gmail.com>
2025-08-22 07:41:47 +0300

    Learn Ease : (CustomEase,elastic,power"1,2,3,4")

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-'use client';
+"use client";
+import { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { CustomEase } from "gsap/CustomEase";
 
-import { useRef } from 'react';
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-
-if (typeof window !== 'undefined') {
+gsap.registerPlugin(CustomEase);
+if (typeof window !== "undefined") {
   gsap.registerPlugin(useGSAP);
 }
 
@@ -16,18 +17,33 @@ export default function Home() {
     tl.current.reversed(!tl.current.reversed());
   };
 
-  useGSAP(
-    () => {
-      const boxes = gsap.utils.toArray('.box');
-      tl.current = gsap
-        .timeline()
-        .to(boxes[0], { x: 120, rotation: 360 })
-        .to(boxes[1], { x: -120, rotation: -360 }, '<')
-        .to(boxes[2], { y: -166 })
-        .reverse();
-    },
-    { scope: container }
-  );
+  useGSAP(() => {
+    const boxes = gsap.utils.toArray<HTMLElement>(".box");
+    gsap.from(boxes, {
+      x: 120,
+      opacity: 0,
+      duration: 2,
+      ease: CustomEase.create(
+        "custom",
+        "M0,0 C0.173,0 0.247,1 0.247,1 0.247,1 0.271,1 0.271,1 0.271,1 0.308,0.51 0.416,0.51 0.502,0.51 0.566,1 0.566,1 0.566,1 0.578,1 0.578,1 0.578,1 0.606,0.783 0.664,0.783 0.71,0.783 0.753,1 0.753,1 0.753,1 0.758,1 0.758,1 0.758,1 0.776,0.913 0.809,0.913 0.836,0.913 0.862,1 0.862,1 0.862,1 0.865,1 0.865,1 0.865,1 0.874,0.968 0.895,0.968 0.912,0.968 0.927,1 0.927,1 0.927,1 0.927,1 0.927,1 0.927,1 0.932,0.989 0.945,0.989 0.956,0.989 0.964,1 0.964,1 0.964,1 0.964,1 0.964,1 0.964,1 0.967,0.997 0.975,0.997 0.982,0.997 0.986,1 0.986,1 0.986,1 0.986,1 0.986,1 0.986,1 0.988,0.999 0.993,0.999 0.995,0.999 0.997,0.889 0.998,0.613 0.998,0.544 0.999,0.104 0.999,0.074 0.999,0.057 1,0.349 1,0.349 "
+      ),
+      repeat: 1,
+      yoyo: true,
+    });
+  });
+
+  // useGSAP(
+  //   () => {
+  //     const boxes = gsap.utils.toArray<HTMLElement>(".box");
+  //     tl.current = gsap
+  //       .timeline()
+  //       .to(boxes[0], { x: 120, rotation: 360 })
+  //       .to(boxes[1], { x: -120, rotation: -360 }, "<")
+  //       .to(boxes[2], { y: -166 })
+  //       .reverse();
+  //   },
+  //   { scope: container }
+  // );
 
   return (
     <main>
@@ -36,9 +52,11 @@ export default function Home() {
         <div>
           <button onClick={toggleTimeline}>Toggle Timeline</button>
         </div>
-        <div className="box gradient-blue">Box 1</div>
-        <div className="box gradient-blue">Box 2</div>
-        <div className="box gradient-blue">Box 3</div>
+        <div className="flex">
+          <div className="box gradient-blue">Box 1</div>
+          <div className="box gradient-blue">Box 2</div>
+          <div className="box gradient-blue">Box 3</div>
+        </div>
       </section>
     </main>
   );
